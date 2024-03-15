2bbf41eddbed60dfb9f8827763a565f107ba6507
avishkar-sapkal <157634351+avishkar-sapkal@users.noreply.github.com>
2024-03-16 00:37:27 +0530

    Update Class.java

--- Class.java
+++ Class.java
@@ -9,7 +9,7 @@ class Student{
 class Class {
     public static void main(String[] args){
         Student s1=new Student();   
-        s1.Show();                                      // s1 is the object of Student class
+        s1.Show();                                                        // s1 is the object of Student class
         System.out.println("The Name of Student is: "+s1.name);           // . is used to give access
         System.out.println("The Roll-no of Student is: "+s1.Roll_no);
     }
