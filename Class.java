3876b92669005b6c49bf3ef66bc56d9ea09ff267
avishkar-sapkal <157634351+avishkar-sapkal@users.noreply.github.com>
2024-03-16 00:36:20 +0530

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,16 @@
+
+class Student{
+    int Roll_no=162;                                                        // Variables
+    String name="Avishkar";
+    void Show(){                                                            //Method
+        System.out.print("Student Class is created succcessfully \n");
+    }
+}
+class Class {
+    public static void main(String[] args){
+        Student s1=new Student();   
+        s1.Show();                                      // s1 is the object of Student class
+        System.out.println("The Name of Student is: "+s1.name);           // . is used to give access
+        System.out.println("The Roll-no of Student is: "+s1.Roll_no);
+    }
+}
